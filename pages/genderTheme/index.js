0589690204c394f3a2603e9c8632837e86dd0828
zx1222 <1655638981@qq.com>
2018-09-20 22:05:18 +0800

    性别选择页 切图

---
+++ pages/genderTheme/index.js
@@ -0,0 +1,54 @@
+// pages/genderTheme/index.js
+Page({
+
+      /**
+       * 页面的初始数据
+       */
+      data: {
+            gender: 2
+      },
+
+      /**
+       * 生命周期函数--监听页面加载
+       */
+      onLoad: function(options) {
+
+      },
+
+      /**
+       * 生命周期函数--监听页面初次渲染完成
+       */
+      onReady: function() {
+
+      },
+
+      /**
+       * 生命周期函数--监听页面显示
+       */
+      onShow: function() {
+
+      },
+
+      /**
+       * 生命周期函数--监听页面隐藏
+       */
+      onHide: function() {
+
+      },
+
+      /**
+       * 生命周期函数--监听页面卸载
+       */
+      onUnload: function() {
+
+      },
+      selectTheme:function(e){
+            console.log(e)
+      },
+      /**
+       * 用户点击右上角分享
+       */
+      onShareAppMessage: function() {
+
+      }
+})
